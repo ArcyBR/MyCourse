5b845a09c386a3a0ba970a5c11bd4289eb1c49e4
Arcangelo Iovino <arcangeloiovino@gmail.com>
2023-02-28 09:24:06 +0100

    Aggiunte view di contenuto ed una view di layout

--- src/Controllers/CoursesController.cs
+++ src/Controllers/CoursesController.cs
@@ -9,11 +9,11 @@ namespace src.Controllers
     public class CoursesController : Controller
     {
         public IActionResult Index(){
-            return Content("sono la Index della Courses");
+            return View();
         }
 
         public IActionResult Detail(string id){
-            return Content($"Sono Detail delle Courses, ho ricevuto l'Id {id}");
+            return View();
         }
     }
 }
